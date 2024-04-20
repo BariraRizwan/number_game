d0052b3e3b80a213a2dcd6baa77fa2ff8c04e743
Barira Rizwan <uk1666464@gmail.com>
2024-04-20 08:23:38 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let  numberIs = Math.random();
+
+const answer = await inquirer.prompt([{
+    name:"guessedNumber",
+    message:"Please guess any number",
+    type:"number",
+}])
+
+if(answer.guessedNumber === numberIs){
+    console.log("Congratulations you guessed a right number");  
+}else{
+    console.log("Oops your guessed number is wrong");
+}
